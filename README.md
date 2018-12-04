c1f5c55f738900c910d2a4f79e3f2e1d6cf561e2
Luuk Lenders <luuklsll@gmail.com>
2018-12-04 23:34:50 +0100

    rudimentary implementation of getting news and doing NLP
    Now also a description on how to get this running in the README.md ;)

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # ARPNLP
+
+
+##Installation
+
+To be able to run this project, you need to have a number of things installed (at least verified with):
+ - Python 3.7
+ - Pipenv, which is used for virtual envs and package management
+ - A C compiler, Google is your friend, should work with Visual Studio C compiler
+ - An API key from newsapi.org, as a developer you can get one for free (limit to 1000 requests/day)
+ 
+After you have installed all of the above, do the following:
+
+ - `pipenv --python3.7` which creates a virtual enviroment
+ - `pipenv install` which installs the packages as defined by the devlopers in the Pipfile
+ - `pipenv run python -m spacy download en` which downloads the english pre-trained models of spacy
+ 
+If one decides to use a normal Virtual Env with pip, or installs system wide, you can also do this using the requirements.txt file. You will still need a C compiler, and run the last command (without the pipenv run)
+
+You also need to create a file API.py, which is only used to define the variable API_KEY (we don't commit that file, it is linked ot you as a person!)
+ 
--- news_retrieval.py
+++ news_retrieval.py
@@ -1,36 +1,56 @@
 from newsapi import NewsApiClient
 from pprint import pprint
 import json
+import spacy
 
 from API import API_KEY
 
-#Init
-newsapi = NewsApiClient(api_key=API_KEY)
+
+def get_news(API_KEY):
+    """"A rudimentary way to retrieve some recent news will be improved upon, but serves purpose so far. Also dumps
+    news in dumpfile.json for possible manual inspection"""
+    # Init tne newsapi client
+    newsapi = NewsApiClient(api_key=API_KEY)
+
+    # Code below is mainly useful for getting the sources in a country, or from a specific type of news category
+    # sources = newsapi.get_sources(language='en', category='general')
+    # print(sources)
+
+    # get_everything requires either a query, sources and domain
+    # articles = newsapi.get_everything(sources='reuters')
+    # print(articles)
+
+    top_articles = newsapi.get_top_headlines(country='ca')
 
 
-# sources = newsapi.get_sources(language='en', category='general')
-#
-# print(sources)
-
-# get_everything requires either a query, sources and domain
-# articles = newsapi.get_everything(sources='reuters')
-# #
-# print(articles)
+    with open('dumpfile.json', 'w') as file:
+        json.dump(top_articles, file, indent=4)
+        print('dumped')
 
 
-top_articles =newsapi.get_top_headlines(country='ca')
+def do_NLP_magic():
+    """"Function to call spaCy NLP magic. Reads from file so we aren't forced to
+    do API calls all the time if we don't want to"""
 
-print(top_articles)
+    # Read the file we just created, as we are in early stages i recommend keeping this here
+    with open('dumpfile.json') as file:
+        x = json.loads(file.read())
+        # print(x)
 
-with open('dumpfile.json', 'w') as file:
-    json.dump(top_articles, file, indent=4)
-    print('dumped')
+    # horrible code, but it makes clear what it does, if not, use prints ;)
+    article = dict((x['articles'][2]))
+    title = (article['title'])
+    description = (article['description'])
+    content = (article['content'])
+    article_contents = (title + "\n" + description + "\n" + content)
+    print(article_contents)
 
-# dictdump = json.dumps(top_articles)
-#
-# with open('dumpfile.txt') as file:
-#     file.write(top_articles)
-#
-# print(dictdump)
+    # Literally the example on the spaCy website, seems to work okay enough for now
+    nlp = spacy.load('en_core_web_sm')
+    doc = nlp(article_contents)
+    for ent in doc.ents:
+        print(ent.text, ent.start_char, ent.end_char, ent.label_)
 
-print('end')
+
+get_news(API_KEY)   #Turn this off so you don't do API calls all the time
+do_NLP_magic()
